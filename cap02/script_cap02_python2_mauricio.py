ac30fad98ef8ac858910a9c69a62a6909fa36edc
Maurício Humberto Vancine <mauricio.vancine@gmail.com>
2017-03-11 00:27:48 -0300

    Update script_cap02_python2_mauricio.py

--- cap02/script_cap02_python2_mauricio.py
+++ cap02/script_cap02_python2_mauricio.py
@@ -8,44 +8,100 @@ como degitar programas e fazer os primeiros testes com operações aritméticas 
 
 Maurício Humberto Vancine
 26/02/2017
+
+Modificado Por Bernardo Niebuhr - 09/03/2017
 """
 
+### 2.5 Os primeiros progrmas
+
 # Listagem 2.1 - Usando o interpretador como calculadora
-2+3
+2 + 3
 
 # Listagem 2.2 - Subtração
-5-3
+5 - 3
 
 # Listagem 2.3 - Adição e subtração
-10-4+2
+10 - 4 + 2
 
 # Listagem 2.4 - Multiplicação e divisão
-2*10
+2 * 10
 
-20/4
+20 / 4
+
+20.0 / 4
+
+20 / 4.0
 
 # Listagem 2.5 - Exponenciação
-2**3
+2 ** 3
 
+# Listagem 2.6 - Resto da divisão inteira
+10 % 3
 
+16 % 7
 
+63 % 8
 
+## Exercício 2.1 ##
+10 + 20 * 30
 
+4 ** 2 / 30
 
+4 ** 2 / 30.0
 
+(9 ** 4 + 2) * 6 - 1
 
+## Exercício 2.2 ##
+10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2
 
+###-----------------------------------------------------------###
 
+### 2.6 Conceitos de variáveis e atribuição
 
+# Listagem 2.7 - O primeiro programa com variáveis
+a = 2
+b = 3
+print a + b
 
+# Listagem 2.8 - Exemplo mostrado no interpretador
+a = 2
+b = 3
+print a + b
 
+# Listagem 2.9 - Outra forma de resolver o problema
+print 2 + 3
 
+# Listagem 2.10 - Outra forma de resolver o problema
+print 5
 
+# Listagem 2.11 - Cálculo de aumento de salário
+salario = 1500
+aumento = 5
+print salario + (salario * aumento / 100)
 
+# Listagem 2.12 - Cálculo de aumento de salário no interpretador
+salario = 1500
+aumento = 5
+print salario + (salario * aumento / 100)
 
+# Listagem 2.13 - Alternativa para o cálculo de aumento de salário
+print(1500 + (1500 * 5 / 100))
 
+## Exercício 2.3 ##
+print("Maurício Humberto Vancine")
 
+## Exercício 2.4 ##
+a = 3
+b = 5
+print(2 * a * 3 * b)
+ 
+## Exercício 2.5 ##
+a = 2
+b = 3
+c = 4
+print a + b + c
 
-
-
-
+## Exercício 2.6 ##
+salario = 750
+aumento = 15
+print salario + (salario * aumento / 100)
