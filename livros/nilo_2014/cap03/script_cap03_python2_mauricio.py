c6768de1bdb6635c0f59bf03c77911cb1976bbfd
Maurício Humberto Vancine <mauricio.vancine@gmail.com>
2017-03-27 23:33:42 -0300

    script 003

--- livros/nilo_2014/cap03/script_cap03_python2_mauricio.py
+++ livros/nilo_2014/cap03/script_cap03_python2_mauricio.py
@@ -9,99 +9,111 @@ erros comuns
 
 
 Maurício Humberto Vancine
-15/03/2017
+20/03/2017
 """
 
-### 2.5 Os primeiros progrmas
+### 3.2 Variáveis numéricas
 
-# Listagem 2.1 - Usando o interpretador como calculadora
-2 + 3
-
-# Listagem 2.2 - Subtração
-5 - 3
-
-# Listagem 2.3 - Adição e subtração
-10 - 4 + 2
-
-# Listagem 2.4 - Multiplicação e divisão
-2 * 10
-
-20 / 4
-
-20.0 / 4
-
-20 / 4.0
-
-# Listagem 2.5 - Exponenciação
-2 ** 3
-
-# Listagem 2.6 - Resto da divisão inteira
-10 % 3
-
-16 % 7
-
-63 % 8
-
-## Exercício 2.1 ##
-10 + 20 * 30
-
-4 ** 2 / 30
-
-4 ** 2 / 30.0
-
-(9 ** 4 + 2) * 6 - 1
-
-## Exercício 2.2 ##
-10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2
+## Exercício 3.1 ##
+5 # inteiro
+5.0 # ponto flutuante
+4.3 # ponto flutuante5
+-2 # inteiro
+100 # inteiro
+1.333 # ponto flutuante
 
 ###-----------------------------------------------------------###
 
-### 2.6 Conceitos de variáveis e atribuição
+### 3.3 Variáveis numéricas
 
-# Listagem 2.7 - O primeiro programa com variáveis
-a = 2
-b = 3
-print a + b
+# Listagem 3.1 - Exemplo de variáveis do tipo lógico
+resultado = True
+aprovado = False
 
-# Listagem 2.8 - Exemplo mostrado no interpretador
-a = 2
-b = 3
-print a + b
+# 3.3.1 Operadores relacionais
+# Listagem 3.2 - Exemplo de uso de operadores relacionais
+a = 1 # a recebe 1
+b = 5 # b recebe 5
+c = 2 # c recebe 2
+d = 1 # d recebe 1
+a == b
+b > a
+a < b
+a == d
+b >= a
+c <= b
+d != a
+d != b
 
-# Listagem 2.9 - Outra forma de resolver o problema
-print 2 + 3
+# Listagem 3.3 - Exemplo de uso de operadores relacionais com variáveis do tipo lógico
+nota = 8
+media = 7
+aprovado = nota > media
+print(aprovado)
 
-# Listagem 2.10 - Outra forma de resolver o problema
-print 5
+## Exercício 3.2 ##
+a = 4
+b = 10
+c = 5.0
+d = 1
+f = 5
 
-# Listagem 2.11 - Cálculo de aumento de salário
+a == c # False
+a < b # True
+d > b # False
+c!= f # False
+a == b # False
+c < d # False
+b > a # True
+c >= f # True
+f >= c # True
+c <= c # True
+c <= f # True
+
+# 3.3.2 Operadores lógicos
+# 3.3.2.1 Operador not
+# Listagem 3.4 - Operador not
+not True
+not False
+
+# 3.3.2.2 Operador and
+# Listagem 3.6 - Operador and
+True and True
+True and False
+False and True
+False and False
+
+# 3.3.2.3 Operador or
+# Listagem 3.6 - Operador or
+True or True
+True or False
+False or True
+False or False
+
+## Exercício 3.3 ##
+a = True
+b = False
+c = True
+
+a and a #True
+b and b # False
+not c # False
+not b # True
+not a # False
+a and b # False
+
+
+
+# 3.3.3 Expressões lógicas
+
+## Exercício 3.4 ##
 salario = 1500
-aumento = 5
-print salario + (salario * aumento / 100)
+salario > 1500
 
-# Listagem 2.12 - Cálculo de aumento de salário no interpretador
-salario = 1500
-aumento = 5
-print salario + (salario * aumento / 100)
 
-# Listagem 2.13 - Alternativa para o cálculo de aumento de salário
-print(1500 + (1500 * 5 / 100))
 
-## Exercício 2.3 ##
-print("Maurício Humberto Vancine")
 
-## Exercício 2.4 ##
-a = 3
-b = 5
-print(2 * a * 3 * b)
- 
-## Exercício 2.5 ##
-a = 2
-b = 3
-c = 4
-print a + b + c
 
-## Exercício 2.6 ##
-salario = 750
-aumento = 15
-print salario + (salario * aumento / 100)
+
+
+###-----------------------------------------------------------###
